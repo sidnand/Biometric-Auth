609970b6a75372ac8a2c354fdd089d288bc8de52
Siddharth Nand <snand233@gmail.com>
2024-06-20 23:34:01 -0700

    user model

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
+from __future__ import annotations
+
 from sqlmodel import SQLModel, Field, create_engine, Session, select
 
-from __future__ import annotations
 from typing import Optional, List
 
 class User(SQLModel, table=True):
