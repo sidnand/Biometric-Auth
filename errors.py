f240ae4afcff1a246f13b9d81d279b385028447f
Siddharth Nand <snand233@gmail.com>
2024-06-21 22:00:35 -0700

    handles reponses and errors

---
+++ errors.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+class Error(Enum):
+    """
+    Enum class for error codes and messages.
+
+    Attributes:
+        USER_NOT_FOUND (tuple): The error code and message for user not found.
+        INTERNAL_SERVER_ERROR (tuple): The error code and message for internal server error.
+    """
+
+    USER_NOT_FOUND = ("user_not_found", "User not found.", 404)
+    INTERNAL_SERVER_ERROR = ("internal_server_error", "Internal server error. Please try again later.", 500)
+    UNAUTHORIZED = ("unauthorized", "Unauthorized", 401)
+
+    def __init__(self, code: str, message: str, http_status: int) -> None:
+        self._code = code
+        self._message = message
+        self._http_status = http_status
+
+    @property
+    def code(self) -> str:
+        return self._code
+
+    @property
+    def message(self) -> str:
+        return self._message
+
+    @property
+    def http_status(self) -> int:
+        return self._http_status
---
+++ response_manager.py
@@ -0,0 +1,25 @@
+from typing import Optional, Union
+from errors import Error
+
+class ResponseManager:
+    @staticmethod
+    def error_response(error: Error, message: str) -> dict:
+        return {
+            "success": False,
+            "error": {
+                "code": error.code,
+                "message": message
+            }
+        }, error.http_status
+
+    @staticmethod
+    def get_error_response(error: Error) -> dict:
+        default_message = "Unknown error occurred."
+        return ResponseManager.error_response(error, error.message)
+
+    @staticmethod
+    def success_response(data: Optional[Union[dict, list]] = None) -> dict:
+        return {
+            "success": True,
+            "data": data
+        }, 200
